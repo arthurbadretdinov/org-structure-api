44ddfd4e78ea42ad8cff26acf006d574f6525100
Artur Badretdinov <artur.badretdinov11@gmail.com>
2026-03-06 20:33:42 +0500

    Setup FastAPI app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+from app.router import router
 from app.database import Base, engine
 from app.models import Department, Employee
 
+app = FastAPI()
+app.include_router(router)
+
 def main():
     Base.metadata.create_all(bind=engine)
+    
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+
 
 if __name__ == "__main__":
     main()
